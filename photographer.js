5aacec66cec506e480de6208e22625772a40cb75
Yada Ly <lyyada@gmail.com>
2021-07-19 11:56:31 +0200

    affichage img et video

--- photographer.js
+++ photographer.js
@@ -6,7 +6,9 @@ fetch("/data/FishEyeData.json")
     const photographer = json.photographers.find(
       (photographer) => photographer.id === id
     );
+    const media = json.media.filter((media) => media.photographerId === id);
 
+    //INFORMATION PHOTOGRAPHE
     //TITLE
     document.title = photographer.name + " - Photographer | FishEye";
 
@@ -28,6 +30,7 @@ fetch("/data/FishEyeData.json")
       const tagsLi = document.createElement("li");
       tagsLi.className = "tag-btn";
       tagsLi.setAttribute("id", TagEl);
+
       const tagsA = document.createElement("a");
       tagsA.setAttribute("href", hrefUrl);
       tagsA.textContent = "#" + TagEl;
@@ -54,6 +57,50 @@ fetch("/data/FishEyeData.json")
     picture.setAttribute("alt", "na");
 
     photographerSection.appendChild(picture);
+    //
+
+    //RECTANGLE LIKE & PRICE
+    /* let likes = 0;
+    media.forEach((media) => (likes += media.likes)); */
+    const likes = media.reduce((likes, media) => likes + media.likes, 0);
+    const total = document.getElementById("totalLikes");
+    total.innerHTML = likes;
+
+    const price = document.getElementById("price");
+    price.innerHTML = photographer.price + "€ / jour";
+    //
+
+    //VIDEOS ET PHOTOS
+    const section = document.getElementById("photographerContent");
+
+    function getSrc(photographer, fileName) {
+      const splitPhotographerName = photographer.name.split(" ");
+      splitPhotographerName.pop();
+      return `../Sample_Photos/${splitPhotographerName.join(" ")}/${fileName}`;
+    }
+
+    function createMediaListItem(photographer, media) {
+      const content = document.createElement("div");
+      content.className = "contentPhotoVideo";
+      const aContentLink = document.createElement("a");
+      aContentLink.setAttribute("href", "#");
+      aContentLink.setAttribute("title", media.title);
+      aContentLink.addEventListener("click", () => console.log(""));
+
+      if (media.image) {
+        const imgElement = document.createElement("img");
+        imgElement.setAttribute("src", getSrc(photographer, media.image));
+        aContentLink.appendChild(imgElement);
+      } else if (media.video) {
+        const videoElement = document.createElement("video");
+        videoElement.setAttribute("src", getSrc(photographer, media.video));
+        aContentLink.appendChild(videoElement);
+      }
+      section.appendChild(content);
+      content.appendChild(aContentLink);
+    }
+
+    media.forEach((media) => createMediaListItem(photographer, media));
   })
   .catch((err) => {
     console.log(err);
