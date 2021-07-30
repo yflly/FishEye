9330df532f273154ebb0cc1189f4ebaad703863a
Yada Ly <lyyada@gmail.com>
2021-07-30 11:04:46 +0200

    lien fichier json

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /*ON RECUPERE LE FICHIER DATA JSON*/
 let data;
-fetch("./data/FishEyeData.json")
+fetch("/data/FishEyeData.json")
   .then((response) => response.json())
   .then((json) => {
     data = json;
@@ -33,7 +33,7 @@ function photographerNodeFactory(photographer) {
   const picture = document.createElement("img");
   picture.setAttribute(
     "src",
-    "./Sample_Photos/Photographers ID Photos/" + photographer.portrait
+    "/Sample_Photos/Photographers ID Photos/" + photographer.portrait
   );
   picture.setAttribute("alt", "na");
 
