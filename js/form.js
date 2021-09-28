18e9cdb6c5348ab651fea04227aaae7fe5a5da9a
Yada Ly <lyyada@gmail.com>
2021-09-28 15:16:35 +0200

    modif dom form

--- js/form.js
+++ js/form.js
@@ -9,78 +9,78 @@ fetch("/data/FishEyeData.json")
     //NAME
     const photographerName = document.getElementById("form-name");
     photographerName.innerHTML = photographer.name;
+
+    // DOM Elements
+    const modalbg = document.querySelector(".bground");
+    const modalBtn = document.querySelectorAll(".modal-btn");
+    //const formData = document.querySelectorAll(".formData");
+    const modalBtnClose = document.querySelectorAll(".bground .close")[0];
+    //const btnSubmit = document.querySelectorAll("#btn-submit");
+    let modalK;
+
+    //Form elements
+    const firstName = document.getElementById("first");
+    const lastName = document.getElementById("last");
+    const email = document.getElementById("email");
+    const message = document.getElementById("message");
+    // launch modal event
+    modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+    // launch modal form
+    function launchModal() {
+      modalbg.style.display = "block";
+      document.getElementById("modal-btn").style.display = "none";
+      modalK = 1;
+      firstName.focus();
+      modalKeyboard();
+
+      firstName.value = "";
+      lastName.value = "";
+      email.value = "";
+      message.value = "";
+    }
+
+    // close modal event
+    modalBtnClose.addEventListener("click", closeModal);
+
+    // close modal form
+    function closeModal() {
+      modalbg.style.display = "none";
+      document.getElementById("modal-btn").style.display = "block";
+      modalK = 0;
+      window.removeEventListener("keydown", keyboardModal);
+    }
+
+    document.forms["modalForm"].addEventListener("submit", handleForSubmit);
+    function handleForSubmit(ev) {
+      ev.preventDefault();
+      console.log(firstName.value + " " + lastName.value + " " + email.value);
+      closeModal();
+    }
+
+    //Si modal ouverte keydown ok
+    function modalKeyboard() {
+      if (modalK == 0) {
+        //console.log(modalK);
+      } else {
+        window.addEventListener("keydown", keyboardModal);
+      }
+    }
+
+    function keyboardModal(evt) {
+      //evt.preventDefault();
+      switch (evt.code) {
+        case "Enter":
+          handleForSubmit();
+          break;
+        case "Escape":
+          closeModal();
+          break;
+        default:
+          return;
+      }
+    }
   })
   .catch((err) => {
     console.log(err);
   });
-
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-//const formData = document.querySelectorAll(".formData");
-const modalBtnClose = document.querySelectorAll(".bground .close")[0];
-//const btnSubmit = document.querySelectorAll("#btn-submit");
-let modalK;
-
-//Form elements
-const firstName = document.getElementById("first");
-const lastName = document.getElementById("last");
-const email = document.getElementById("email");
-const message = document.getElementById("message");
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
-  document.getElementById("modal-btn").style.display = "none";
-  modalK = 1;
-  firstName.focus();
-  modalKeyboard();
-
-  firstName.value = "";
-  lastName.value = "";
-  email.value = "";
-  message.value = "";
-}
-
-// close modal event
-modalBtnClose.addEventListener("click", closeModal);
-
-// close modal form
-function closeModal() {
-  modalbg.style.display = "none";
-  document.getElementById("modal-btn").style.display = "block";
-  modalK = 0;
-  window.removeEventListener("keydown", keyboardModal);
-}
-
-document.forms["modalForm"].addEventListener("submit", handleForSubmit);
-function handleForSubmit(ev) {
-  ev.preventDefault();
-  console.log(firstName.value + " " + lastName.value + " " + email.value);
-  closeModal();
-}
-
-//Si modal ouverte keydown ok
-function modalKeyboard() {
-  if (modalK == 0) {
-    //console.log(modalK);
-  } else {
-    window.addEventListener("keydown", keyboardModal);
-  }
-}
-
-function keyboardModal(evt) {
-  //evt.preventDefault();
-  switch (evt.code) {
-    case "Enter":
-      handleForSubmit();
-      break;
-    case "Escape":
-      closeModal();
-      break;
-    default:
-      return;
-  }
-}
